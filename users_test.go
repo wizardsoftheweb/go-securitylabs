51d5fbba2c83ea61b43433567e91e07eeff22d43
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-23 13:39:36 -0500

    Build PUT /api/users/:id handler

--- users_test.go
+++ users_test.go
@@ -20,6 +20,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/hetiansu5/urlquery"
@@ -34,6 +35,8 @@ const (
 	testLimitMax          = 50
 	testPhraseMax         = 50
 	testTotal             = 100
+	testNonexistentUserId = "5f5f18439qqq493352660d28"
+	testNameMax           = 25
 )
 
 var (
@@ -56,6 +59,11 @@ var (
 	testValidSortTypes = []string{
 		"ASC",
 		"DESC",
+	}
+	testExistingUserIds = []string{
+		"3bd68695e165af6ced227afc",
+		"5f5f18439dad493352660d28",
+		testNonexistentUserId,
 	}
 )
 
@@ -242,6 +250,56 @@ func usersGetUsersDetailsTestHandler(w http.ResponseWriter, r *http.Request) {
     }]
   }]
 }`, string(pagesBytes))))
+}
+
+// /api/users/:id
+// responses pulled from
+// https://apidocs.hunter2.com/#put-user
+// I have no idea if these are actually what the API returns
+func handlerPutUser(w http.ResponseWriter, r *http.Request) {
+	var user PutUser
+	parseError := json.NewDecoder(r.Body).Decode(&user)
+	// TODO: is this the correct error?
+	if nil != parseError {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("{\"message\":\"Invalid request body\"}"))
+		return
+	}
+	userId := strings.Replace(r.URL.RequestURI(), "/api/user/", "", 1)
+	// TODO: How to test ID validity?
+	if 24 != len(userId) {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("{\"message\":\"User id is invalid\"}"))
+		return
+	}
+	if !listContains(testExistingUserIds, userId) {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("{\"message\":\"User not found\"}"))
+		return
+	}
+	// TODO: How to check email validity?
+	if "" == user.Email {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("{\"message\":\"Email is invalid\"}"))
+		return
+	}
+	if testNameMax < len(user.Name) {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("{\"message\":\"Name must be less than 25 characters\"}"))
+		return
+	}
+	// TODO: How to check role ids not being an array?
+	// TODO: Does the endpoint actually check valid role ids?
+	for _, role := range user.RoleIds {
+		if !listContains(testRoleIds, role) {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("{\"message\":\"Invalid roleIds\"}"))
+			return
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+	responseBody, _ := json.Marshal(user)
+	_, _ = w.Write(responseBody)
 }
 
 func TestClient_GetUsers(t *testing.T) {
